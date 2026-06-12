82b6f4a994e617552873115b5af6719286607478
strestik <matejstrestik@gmail.com>
2026-06-12 09:30:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function getRndInteger(min, max) {
   }
 
 function startSimulation() {
-    setInterval(function() {
-        ctx.fillRect(getRndInteger(1, 600), getRndInteger(1, 400), 7, 7);
-    }, 100);
+    let limit = 0.5;
+    
+        setInterval(function() {
+            let pointX = getRndInteger(1, 400);
+            let pointY = getRndInteger(1, 600);        
+            if (intensity(pointX, 1200) > limit)
+            {
+                ctx.fillRect(pointX, pointY, 7, 7);
+            }
+        }, 100);
+}
+
+function intensity(y, canvasHeight) {
+    let centered = y - canvasHeight / 2;
+    let k = 0.05;        // stripe spacing
+    let sigma = 150;     // gaussian width
+    
+    let cos2 = Math.cos(k * centered) ** 2;
+    let gaussian = Math.exp(-(centered ** 2) / (2 * sigma ** 2));
+    
+    return cos2 * gaussian;
 }
